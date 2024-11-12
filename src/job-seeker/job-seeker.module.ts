123c72e45c6c0543abbc8aa33804a1e18a4d07d0
Timothy <oludefiyinfoluwa06@gmail.com>
2024-11-12 11:38:43 +0100

    add job seeker model to module

--- src/job-seeker/job-seeker.module.ts
+++ src/job-seeker/job-seeker.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JobSeekerService } from './job-seeker.service';
 import { JobSeekerController } from './job-seeker.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JobSeeker, JobSeekerSchema } from './schema/job-seeker.schema';
 
 @Module({
+  imports:[
+    MongooseModule.forFeature([
+      { name: JobSeeker.name, schema: JobSeekerSchema }
+    ]),
+  ],
   controllers: [JobSeekerController],
   providers: [JobSeekerService],
 })
